0e5ee8a0b8ca6c7d534f298b5ab9dc96c774b24c
Enamul Haque <enamul_haque@citybank.com>
2024-04-22 17:39:46 +0600

    first commit

--- src/main/java/com/example/springbatch3/repository/ChargeDetailsRepo.java
+++ src/main/java/com/example/springbatch3/repository/ChargeDetailsRepo.java
@@ -3,10 +3,7 @@ package com.example.springbatch3.repository;
 
 import com.example.springbatch3.entity.ChargeDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface ChargeDetailsRepo extends JpaRepository<ChargeDetails, Long> {
--- src/main/java/com/example/springbatch3/repository/CorpApprovedTransactionRepo.java
+++ src/main/java/com/example/springbatch3/repository/CorpApprovedTransactionRepo.java
@@ -3,12 +3,8 @@ package com.example.springbatch3.repository;
 
 import com.example.springbatch3.entity.TestApprovedTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Transactional
 @Repository
